3ecff1a83a48c9df10d7a8a65d7d5bf24cc69cd8
nirasan <niranirao@gmail.com>
2012-10-18 16:50:26 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RendaServer::Application.config.secret_token = '4b88e8023e0946d3f48e3ffcece21139dd6457e36b9dc84429aad2ebf1eb750a92d0d2231549117e9f56a5a9e427e6710af4da081bd9f97e2c7b551719c420b4'
