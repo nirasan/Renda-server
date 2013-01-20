e1040f58994eeed2676b5425c1f995754245447d
nirasan <niranirao@gmail.com>
2013-01-20 20:41:34 +0900

    ランキング調整

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,17 +1,29 @@
 
 class RankingController < ApplicationController
+  
   def personal
     user = User.find_by_mail_address_and_access_token(params[:mail_address], params[:access_token])
     return head :bad_request unless user.present?
+    
+    data = _make_ranking_data(Ranking.personal_top(user_id: user.id))
+    render :json => data
+  end
 
-    rankings = Ranking.personal_top(user_id: user.id)
+  def general
+    user = User.find_by_mail_address_and_access_token(params[:mail_address], params[:access_token])
+    return head :bad_request unless user.present?
 
-    # 順位をつける
-    # countが同じなら同順に
+    data = _make_ranking_data(Ranking.general_top)
+    render :json => data
+  end
+
+  # 順位をつける
+  # countが同じなら同順に
+  def _make_ranking_data(rankings)
     rank       ||= 0
     real_rank  ||= 0
     prev_count ||= 0
-    data = rankings.map do |r|
+    return rankings.map do |r|
       real_rank += 1
       if r.count != prev_count
         rank = real_rank
@@ -19,10 +31,5 @@ class RankingController < ApplicationController
       prev_count = r.count
       {username: r.user.username, count: r.count, rank: rank}
     end
-
-    render :json => data
-  end
-
-  def general
   end
 end
